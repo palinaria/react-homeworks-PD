9cdb8ff6083584954d889fe8f5935a0d3230f840
palinaria <167099280+palinaria@users.noreply.github.com>
2025-03-30 14:20:24 +0300

    Revert "Homework 1:Remove unnecessary code.Create a basic structure of your p…"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const projectInfo = {
-    description: "Project.Palina Dolbik.Have a nice day!"
-  };
-
-
-  const items = ["El1", "El2", "El3"];
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>{projectInfo.description}</h1>
-      <ul>  
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
-
+export default App
